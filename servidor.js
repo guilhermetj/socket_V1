20889d5a6f515c10d2aee50a8c7278542f181337
guilhermetj <guips1998@gmail.com>
2022-09-30 14:58:07 -0300

    1

--- servidor.js
+++ servidor.js
@@ -6,7 +6,7 @@ const Pacote = require('./Pacote');
 let server = net.createServer();
 const buffer = Buffer.alloc(3);
 server.on('connection', handleConnection);
-server.listen(5353,'127.0.0.1', function() {
+server.listen(5353,'127.0.0.1', function() {tc
   console.log('server listening to %j', server.address());
 });
 function handleConnection(conn) {
@@ -16,7 +16,7 @@ function handleConnection(conn) {
   conn.once('close', onConnClose);
   conn.on('error', onConnError);
 
-  function onConnData(d) {
+  function  onConnData(d) {
       console.log('connection data from %s: %j', remoteAddress, d.toString('hex'));
       console.log('Comprimento do Pacote: %j', d[1]);
       console.log('Versão do Hardware: %j', d[4]);
@@ -25,22 +25,20 @@ function handleConnection(conn) {
       //console.log('IMEI: %s', imei);
       var crcCalc = Buffer.from(d.buffer,0,(d.length - 2));
       var sum = crc16.checkSum(crcCalc, {retType: 'buffer'});
-      var tipo_pacote = d[0] == 0x01 ? 'head' : 'Body';
-      let pacote = {pacote_blob : d, 
-                    pacote_varbinary : d, 
-                    tipo_pacote : tipo_pacote, 
-                    pacote_hex: d.toString('hex'),
-                    buffer_pacote : Buffer.from(d.buffer,0,d.length ), 
-                    crc: 0x02 +sum.toString('hex')};
-      SalvarPacote(pacote);
+      //var tipo_pacote = d[0] == 0x01 ? 'head' : 'Body';
+      //let pacote = {pacote_blob : d, 
+       //             pacote_varbinary : d, 
+       //             tipo_pacote : tipo_pacote, 
+       //             pacote_hex: d.toString('hex'),
+       //             buffer_pacote : Buffer.from(d.buffer,0,d.length ), 
+        //            crc: 0x02 +sum.toString('hex')};
+      //SalvarPacote(pacote);
       //SalvarBanco()
-      console.log(pacote);
       console.log('result: ' + 0x02 + sum.toString('hex'));
       buffer[0] = 0x02;
       buffer[1] = sum[0];
       buffer[2] = sum[1];
       conn.write(buffer);
-    
   }
   function onConnClose() {
     console.log('connection from %s closed', remoteAddress);
